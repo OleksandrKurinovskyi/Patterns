88a70a25bed2d727e280a711ea107355b9c962f5
Oleksandr Kurinovskyi <o.kurinovskyi@gmail.com>
2019-11-03 13:28:16 +0200

    Added singlton

---
+++ Singlton/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using TitleLib;
+using System.Reflection;
+using System.Threading;
+
+namespace Singlton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Title.UpperIndent(Assembly.GetExecutingAssembly());
+
+            //TestSimple();
+
+            TestThresd();
+
+
+            Title.BottonIndent();
+        }
+
+        static void TestSimple()
+        {
+            SingltonClass s1 = SingltonClass.GetInstance();
+            Console.WriteLine(s1.GetHashCode());
+            Console.WriteLine(SingltonClass.Counter);
+
+            SingltonClass s2 = SingltonClass.GetInstance();
+            Console.WriteLine(s2.GetHashCode());
+            Console.WriteLine(SingltonClass.Counter);
+        }
+
+        static void TestThresd()
+        {
+            (new Thread(() =>
+            {
+                SingltonClass s1 = SingltonClass.GetInstance();
+                Console.WriteLine(s1.GetHashCode());
+                Console.WriteLine(SingltonClass.Counter);
+            })).Start();
+            SingltonClass s2 = SingltonClass.GetInstance();
+            Console.WriteLine(s2.GetHashCode());
+            Console.WriteLine(SingltonClass.Counter);
+        }
+    }
+}
---
+++ Singlton/SingltonClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singlton
+{
+    class SingltonClass
+    {
+        private static Object _locker = new object();
+
+        public static int Counter { get; private set; }
+
+        private static SingltonClass _singltonClass;
+
+        public static SingltonClass GetInstance()
+        {
+            if (_singltonClass == null)
+            {
+                lock(_locker)
+                {
+                    if (_singltonClass == null)
+                    {
+                        _singltonClass = CreateInstance();
+                    }
+                }
+            }
+            return _singltonClass;
+        }
+
+        private SingltonClass()
+        {
+
+        }
+
+        private static SingltonClass CreateInstance()
+        {
+            Counter++;
+            return new SingltonClass();
+        }
+    }
+}
