5026b9a04e1202976e8ceed6c68f58aa264077b6
Oleksandr Kurinovskyi <o.kurinovskyi@gmail.com>
2019-11-07 05:58:22 +0200

    Started adding Facade

---
+++ Facade/Program.cs
@@ -0,0 +1,15 @@
+﻿using TitleLib;
+using System.Reflection;
+
+namespace Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Title.UpperIndent(Assembly.GetExecutingAssembly());
+
+            Title.BottonIndent();
+        }
+    }
+}
