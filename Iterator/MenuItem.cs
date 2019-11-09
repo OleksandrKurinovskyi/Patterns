b8c0833f63881dba35bb1e33e7cb2e743e1cf3a3
Oleksandr Kurinovskyi <o.kurinovskyi@gmail.com>
2019-11-09 10:54:19 +0200

    In adding Iterator

---
+++ Iterator/MenuItem.cs
@@ -0,0 +1,18 @@
+﻿namespace Iterator
+{
+    class MenuItem
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public bool IsVegin { get; private set; }
+        public float Price { get; private set; }
+
+        public MenuItem(string name, string description, bool isVegin, float price)
+        {
+            Name = name;
+            Description = description;
+            IsVegin = isVegin;
+            Price = price;
+        }
+    }
+}
